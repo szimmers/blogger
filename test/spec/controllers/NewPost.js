ef8d4c2a5ab4b7d3fb9923736e50646df2846a9f
Steve Zimmers <szimmers@digitalprimates.net>
2013-10-09 11:07:15 -0500

    unit tests
    - 100% coverage for controllers

--- test/spec/controllers/NewPost.js
+++ test/spec/controllers/NewPost.js
@@ -57,4 +57,54 @@ describe('Controller: NewPostCtrl', function () {
 			expect(location.path()).toBe('/');
 		}));
 	});
+
+	describe('when the system cannot post a new entry', function() {
+
+		beforeEach(module('bloggerApp'));
+
+		var NewpostCtrl, scope, location;
+
+		// mock for BlogPost service
+		beforeEach(function () {
+
+			var saveDeferred;
+
+			var mockBlogPostService = {
+				create: function(entry) {
+					saveDeferred.reject('some random issue');
+					return saveDeferred.promise;
+				}
+			};
+
+			module(function ($provide) {
+				$provide.value('BlogPosts', mockBlogPostService);
+			});
+
+			inject(function($q) {
+				saveDeferred = $q.defer();
+			})
+		});
+
+		beforeEach(inject(function ($controller, $rootScope, $location) {
+
+			scope = $rootScope.$new();
+
+			location = $location;
+
+			NewpostCtrl = $controller('NewPostCtrl', {
+				$scope: scope
+			});
+		}));
+
+		it ('should keep me on the same page', inject(function() {
+
+			scope.entry = "I like soup.";
+
+			scope.postNewEntry();
+
+			scope.$apply();
+
+			expect(location.path()).not.toBe('/');
+		}));
+	});
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,29 +2,168 @@
 
 describe('Controller: MainCtrl', function () {
 
-	// load the controller's module
-	beforeEach(module('bloggerApp'));
+	describe('when the controller is loaded', function() {
 
-	var MainCtrl,
-		scope;
+		// load the controller's module
+		beforeEach(module('bloggerApp'));
 
-	// Initialize the controller and a mock scope
-	beforeEach(inject(function ($controller, $rootScope) {
-		scope = $rootScope.$new();
+		var MainCtrl,
+			scope;
 
-		var posts = [
-			{'uniqueId':1, 'creationDate': '2013-08-10', 'entry':'i like soup'},
-			{'uniqueId':2, 'creationDate': '2013-08-10', 'entry':'soup is good food.'},
-			{'uniqueId':3, 'creationDate': '2013-08-08', 'entry':'some hats are shaped like oklahoma\nsome hats are shaped like the Zuiderzee!'}
-		];
+		// Initialize the controller and a mock scope
+		beforeEach(inject(function ($controller, $rootScope) {
+			scope = $rootScope.$new();
 
-		MainCtrl = $controller('MainCtrl', {
-			$scope: scope,
-			posts: posts
+			var posts = [
+				{'uniqueId':1, 'creationDate': '2013-08-10', 'entry':'i like soup'},
+				{'uniqueId':2, 'creationDate': '2013-08-10', 'entry':'soup is good food.'},
+				{'uniqueId':3, 'creationDate': '2013-08-08', 'entry':'some hats are shaped like oklahoma\nsome hats are shaped like the Zuiderzee!'}
+			];
+
+			MainCtrl = $controller('MainCtrl', {
+				$scope: scope,
+				posts: posts
+			});
+		}));
+
+		it('should attach the post list to the scope', function () {
+			expect(scope.posts.length).toBe(3);
 		});
-	}));
-
-	it('should attach the post list to the scope', function () {
-		expect(scope.posts.length).toBe(3);
 	});
+
+	describe('when i want to delete posts', function() {
+
+		// load the controller's module
+		beforeEach(module('bloggerApp'));
+
+		var MainCtrl,
+			scope,
+			posts = [];
+
+		// mock for BlogPost service
+		beforeEach(function () {
+
+			var deleteDeferred;
+
+			var mockBlogPostService = {
+				delete: function(post) {
+					// don't really care which one we delete, just need to delete one
+					posts.pop();
+					deleteDeferred.resolve(posts);
+					return deleteDeferred.promise;
+				},
+				deleteAll: function() {
+					posts.length = 0;
+					deleteDeferred.resolve(posts);
+					return deleteDeferred.promise;
+				}
+			};
+
+			module(function ($provide) {
+				$provide.value('BlogPosts', mockBlogPostService);
+			});
+
+			inject(function($q) {
+				deleteDeferred = $q.defer();
+			})
+		});
+
+		// Initialize the controller and a mock scope
+		beforeEach(inject(function ($controller, $rootScope) {
+			scope = $rootScope.$new();
+
+			posts = [
+				{'uniqueId':1, 'creationDate': '2013-08-10', 'entry':'i like soup'},
+				{'uniqueId':2, 'creationDate': '2013-08-10', 'entry':'soup is good food.'},
+				{'uniqueId':3, 'creationDate': '2013-08-08', 'entry':'some hats are shaped like oklahoma\nsome hats are shaped like the Zuiderzee!'}
+			];
+
+			MainCtrl = $controller('MainCtrl', {
+				$scope: scope,
+				posts: posts
+			});
+		}));
+
+		it('deleting a single post should result in 1 fewer posts', function () {
+			scope.deletePost(posts[0]);
+
+			scope.$apply();
+
+			expect(scope.posts.length).toBe(2);
+		});
+
+		it('deleting all the posts should result in 0 posts', function () {
+			scope.deleteAll();
+
+			scope.$apply();
+
+			expect(scope.posts.length).toBe(0);
+		});
+	});
+
+	describe('when i want to delete posts', function() {
+
+		// load the controller's module
+		beforeEach(module('bloggerApp'));
+
+		var MainCtrl,
+			scope;
+
+		// mock for BlogPost service
+		beforeEach(function () {
+
+			var deleteDeferred;
+
+			var mockBlogPostService = {
+				delete: function(post) {
+					// don't really care which one we delete, just need to delete one
+					deleteDeferred.reject('cannot delete');
+					return deleteDeferred.promise;
+				},
+				deleteAll: function() {
+					deleteDeferred.reject('cannot delete');
+					return deleteDeferred.promise;
+				}
+			};
+
+			module(function ($provide) {
+				$provide.value('BlogPosts', mockBlogPostService);
+			});
+
+			inject(function($q) {
+				deleteDeferred = $q.defer();
+			})
+		});
+
+		// Initialize the controller and a mock scope
+		beforeEach(inject(function ($controller, $rootScope) {
+			scope = $rootScope.$new();
+
+			var posts = [];
+
+			MainCtrl = $controller('MainCtrl', {
+				$scope: scope,
+				posts: posts
+			});
+		}));
+
+		it('failure to delete a single post should result in an alert', function () {
+			spyOn(window, "alert");
+			scope.deletePost({});
+
+			scope.$apply();
+
+			expect(window.alert).toHaveBeenCalled();
+		});
+
+		it('failure to delete all posts should result in an alert', function () {
+			spyOn(window, "alert");
+			scope.deleteAll();
+
+			scope.$apply();
+
+			expect(window.alert).toHaveBeenCalled();
+		});
+	});
+
 });
